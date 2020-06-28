4b90c940b53f52ec188c2689efb66dd212d78ea0
tonitdiep <adiep123@gmail.com>
2020-06-28 08:12:48 -0400

    establish /users/signup.erb and form between user_controoler.rb and signup.erb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,6 @@
 class UsersController < ApplicationController
-
+    get '/users/signup' do  #first route to render my signup form, not processing anything or receiving params from any form
+        "this is the signup route, dahhling"
+        erb :'/users/signup'
+    end
 end
