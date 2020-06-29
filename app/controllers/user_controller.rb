e89fb6b0a6984a8637c2609bf40e1facd6bc984a
tonitdiep <adiep123@gmail.com>
2020-06-28 22:20:23 -0400

    fixing form syntax in login.erb and ssignup.erb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,14 +1,31 @@
 class UsersController < ApplicationController
     get '/users/signup' do  #first route to render my signup form, not processing anything or receiving params from any form
-        "this is the signup route, dahhling"
+        # "this is the signup route, dahhling"
         erb :'/users/signup'
     end
-    post '/users/signup' do  #first route to render my signup form, not processing anything or receiving params from any form
+    post '/users/signup' do  
+        #first route to render my signup form, not processing anything or receiving params from any form
         #process and create new object
         @user = User.create(username: params[:username], password: params[:password])
-        # binding.pry
         # params.inspect
+        redirect '/products'
     end
+
+    get '/products'
+        @products = Product.all
+        erb :index
+    end
+
+    get '/users/login' do 
+        # "Please login"
+        erb :'/users/login'
+    end
+
+    post '/users/login' do 
+        @user = User.create(username: params[:username], password: params[:password])
+        redirect '/objects'
+    end
+
 end
 
 #remember for usage in the contorller
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
     has_many :products
     has_secure_password   #login/logout/README
-
-
 end 
